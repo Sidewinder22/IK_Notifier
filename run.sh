faa22e3bca9168cf223cb6adee7d3ae90256ca8b
Sidewinder22 <globemaster787@gmail.com>
2018-03-26 16:33:55 +0200

    Spider is working. Actually spider extract only names of thread from web page

--- run.sh
+++ run.sh
@@ -3,18 +3,18 @@
 if [ -z $2 ]
 then
     echo -e "Not enough arguments!"
-<<<<<<< HEAD
-    echo -e "Using: \n\t$ ./run.sh -a login_param=[USER] -a password_param=[PASSWORD]"
-=======
-    echo -e "Using: \n\t$ ./run.sh [USER] [PASSWORD]"
->>>>>>> 7c4eb956d69fb76b47512523c272e557b9e55ce0
+    echo -e "Using: \n\t$ ./run.sh \"[USER]\" \"[PASSWORD]\""
     exit 1
 fi
 
 echo -e "########### {\_Sidewinder22_/} ############"
 echo -e "IK_Spider\n"
 
-scrapy runspider src/scraper.py -a login_param=$1 -a password_param=$2 
+#while true;
+#    do
+    scrapy runspider src/scraper.py -a login_param=$1 -a password_param=$2 
+#    sleep 60
+#done
 
 echo -e "\n########### {\_Sidewinder22_/} ###########"
 echo -e "Bye..."
--- src/scraper.py
+++ src/scraper.py
@@ -8,51 +8,49 @@ Notify.init("last_unread_msgs_spider")
 
 class MyWebSpider(scrapy.Spider):
     name = "last_unread_msgs_spider"
-    start_urls = ['http://instrumentyklawiszowe.com/index.php?action=login']
+    start_urls = ['http://instrumentyklawiszowe.com/index.php']
 
     def parse(self, response):
-        self.log("##################################### start")
-
         return scrapy.FormRequest.from_response(
             response,
             callback=self.after_login,
-            formdata={'user': 'self.login_param', 'passwrd': 'self.password_param'}
+            formdata={'user': self.login_param, 'passwrd': self.password_param}
         )
 
     def after_login(self, response):
         if 'Hasło niewłaściwe' in response.body.decode():
             self.log("Login failed!")
             return
-        else:
-            self.log("Login successful")
-            return Request(url="http://instrumentyklawiszowe.com/",
-               callback=self.parse_main_page)
 
-    def parse_main_page(self, response):
-        if 'REGULAMIN FORUM' in response.body.decode():
-            self.log("++++ done!")
+        if 'Nazwa użytkownika nie istnieje.' in response.body.decode():
+            self.log("User doesn't exist!")
+            return
+
+        self.log("Login successful")
+        return Request(url="http://instrumentyklawiszowe.com/index.php?action=unread",
+           callback=self.process_page)
+
+
+    def process_page(self, response):
+        self.log("process_page")
+        SET_SELECTOR = '.windowbg2'
+        NAME_SELECTOR = 'span a ::text'
+
+        for thing in response.css(SET_SELECTOR):
+
+            label =  thing.css(NAME_SELECTOR).extract_first(),
+            result = ''
+            print (label)
+            if label[0]:
+                print(label[0])
+                result += label[0]
+                result += ', '
 
             Message = Notify.Notification.new(
-                "IK_Spider",
-                "Hi, I'm logged!",
+                "Forum IK.com - nowe wątki",
+                # "kom",
+                result,
                 "dialog-information"
             )
 
             Message.show()
-            return Request(url="http://instrumentyklawiszowe.com/index.php?action=unread",
-               callback=self.process_page)
-
-        else:
-            self.log("#### not done!")
-            return
-
-    def process_page(self, response):
-        self.log("process_page")
-        SET_SELECTOR = '.subject  windowbg2'
-        for thing in response.css(SET_SELECTOR):
-
-            self.log("thing")
-            NAME_SELECTOR = 'id a ::text'
-            yield {
-                'name': thing.css(NAME_SELECTOR).extract_first(),
-            }
